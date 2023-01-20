d2e5c56c58aafc5e756176efaf0d1dec68a88452
Claudio-Olivera <claudioolivera753@gmail.com>
2023-01-20 18:32:12 -0300

    strict true, components created, activated -o

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+
+import { TaskComponent } from './components/task/task.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    TaskComponent,
+    TaskFormComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
